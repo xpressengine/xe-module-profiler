43bf4d1405b500be94040c1b525ed7a780fb013b
bnu <bnufactory@gmail.com>
2014-08-27 17:46:13 +0900

    #2 slow log select/insert 추가 * `XE.writeSlowlog` 트리거를 사용하여 trigger 로그를 수집   * 현재 설정이 없는 상태이므로 모든 동작을 기록 함

--- profiler.admin.model.php
+++ profiler.admin.model.php
@@ -6,5 +6,30 @@ class profilerAdminModel extends profiler
 	function init()
 	{
 	}
+
+	/**
+	 * slowlog 데이터 반환
+	 *
+	 * @param stdClass $args
+	 * @return array
+	 */
+	function getStaticsSlowlog($type, $args = null)
+	{
+		$cond = new stdClass;
+		$cond->type = $type;
+		$cond->hash_id = $args->hash_id;
+
+		// $cond->like_caller = $args->like_caller;
+		// $cond->like_called = $args->like_called;
+
+		$cond->start = $args->start;
+		$cond->end = $args->end;
+
+		$output = executeQueryArray('profiler.getStatisticSlowlog', $cond);
+		if(!$output->data) $output->data = array();
+
+		return $output;
+	}
 }
 /* End of file */
+
